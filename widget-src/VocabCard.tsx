f04f3fb91d8d873f798d45bb5a9f103959d88d5f
Hongyi Hu <hhu@figma.com>
2023-08-10 12:24:59 -0400

    update find sentences icon

--- widget-src/VocabCard.tsx
+++ widget-src/VocabCard.tsx
@@ -1,7 +1,7 @@
 import * as nwi from "./nwInterfaces";
 import * as util from "./Utils"
 const {widget} = figma;
-const {AutoLayout, Rectangle, Text, Frame, Image} = widget;
+const {AutoLayout, Rectangle, Text, Frame, Image, SVG} = widget;
 
 type VocabCardProps = {
     nodeInfo: nwi.Daum
@@ -93,14 +93,24 @@ export default function VocabCard({nodeInfo}: VocabCardProps) {
             }}
             verticalAlignItems="center"
         >
-            <Image
+                <SVG
+                    name="Search"
+                    x={9.664}
+                    y={9.664}
+                    height={13}
+                    width={13}
+                    src="<svg width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <path fill-rule='evenodd' clip-rule='evenodd' d='M9.90112 6.08601C9.90112 8.53227 7.91803 10.5154 5.47177 10.5154C3.0255 10.5154 1.04241 8.53227 1.04241 6.08601C1.04241 3.63974 3.0255 1.65666 5.47177 1.65666C7.91803 1.65666 9.90112 3.63974 9.90112 6.08601ZM8.87763 10.0613C7.96215 10.8464 6.77235 11.3207 5.47177 11.3207C2.58073 11.3207 0.237076 8.97705 0.237076 6.08601C0.237076 3.19497 2.58073 0.851318 5.47177 0.851318C8.36281 0.851318 10.7065 3.19497 10.7065 6.08601C10.7065 7.38658 10.2322 8.57638 9.44709 9.49186L13.0045 13.0493L12.4351 13.6188L8.87763 10.0613Z' fill='white' fill-opacity='0.8'/>
+                    </svg>" 
+                />
+            {/* <Image
                 name="Search"
                 blendMode="color-dodge"
                 strokeWidth={1.464}
                 width={23.431}
                 height={23.431}
-                src="<Add image URL here>"
-            />
+                src=""
+            /> */}
             <Text
                 name="Find sentences"
                 fill="#FFF"
